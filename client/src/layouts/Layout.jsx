b8ac7c9a6647452766075de8be65afcc82cfd31f
Ali Eren Gündüz <aegakademi@gmail.com>
2024-04-02 13:01:01 +0300

    .

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import MainLayout from "./MainLayout";
 import AdminLayout from "./AdminLayout";
 
-const isAdmin = window.location.pathname.startsWith('/admin')
+const isAdmin =  window.location.pathname.includes('/admin')
 export const Layout = isAdmin ? AdminLayout : MainLayout
